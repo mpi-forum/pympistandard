6959201fc98b5cb50a78b06e024236ca977af060
Martin Ruefenacht <martin.ruefenacht@par-tec.com>
2024-02-05 17:21:20 +0100

    Fixed noxfile

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,9 @@
+import os
 import nox
+
+os.environ.update({"PDM_IGNORE_SAVED_PYTHON": "1"})
 
 @nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
 def tests(session):
-    session.install(".[tests]")
-
-    session.run("pytest", *session.posargs)
+    session.run_always("pdm", "install", "-G", "test", external=True)
+    session.run("pytest")
