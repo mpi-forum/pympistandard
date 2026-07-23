3baf0ccd77e7942d9d99b09d4d662a25d33adf2d
Martin Rüfenacht <martin@hzero.tech>
2026-07-23 08:30:02 +0200

    version bump to 0.2

--- src/pympistandard/__init__.py
+++ src/pympistandard/__init__.py
@@ -5,7 +5,7 @@ below this level should be considered private.
 
 __all__ = ["PROCEDURES", "KINDS", "CALLBACKS", "PREDEFINED_FUNCTIONS"]
 __author__ = "Martin Ruefenacht"
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 
 
 from pathlib import Path
